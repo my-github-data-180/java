8cc1ca7920d288aca8b8660d8bc309806ddd531c
naveenp7461 <nrpme107@gmail.com>
2023-10-12 22:42:31 +0530

    Assignment -1 Q4 uploaded and updated

---
+++ ASSIGN-1/Q_Pattern/src/Pattern.java
@@ -0,0 +1,28 @@
+
+public class Pattern {
+
+	public static void main(String[] args) {
+	
+     int x=2;
+     for(int i=0;i<x;i++)
+     {
+    	 for(int j=0;j<x; j++)
+    	 {
+    		  if(j<x-j-1)
+    		  {
+    			  System.out.print(" ");
+    		  }
+    		  else
+    		  {
+    			  System.out.print("* ");
+    		  }
+    	 }
+    	 System.out.println("");
+     }
+
+	}
+
+}
+public class Pattern {
+
+}
